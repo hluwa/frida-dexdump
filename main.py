5ef4579ad751cf5bd37f96b4c9e57b3c3ad5200a
hluwa <hluwa888@gmail.com>
2020-05-26 21:57:51 +0800

    optimize python logic.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # Author: hluwa <hluwa888@gmail.com>
 # HomePage: https://github.com/hluwa
 # CreatedTime: 2020/1/7 20:57
-
+import hashlib
 import os
 import random
 import sys
+
+try:
+    from shutil import get_terminal_size as get_terminal_size
+except:
+    from backports.shutil_get_terminal_size import get_terminal_size as get_terminal_size
 
 import click
 import frida
@@ -29,10 +34,12 @@ banner = """
 ----------------------------------------------------------------------------------------
 """
 
+md5 = lambda bs: hashlib.md5(bs).hexdigest()
+
 
 def show_banner():
     try:
-        if os.get_terminal_size().columns >= len(banner.splitlines()[1]):
+        if get_terminal_size().columns >= len(banner.splitlines()[1]):
             for line in banner.splitlines():
                 click.secho(line, fg=random.choice(['bright_red', 'bright_green', 'bright_blue', 'cyan', 'magenta']))
     except:
@@ -43,7 +50,7 @@ def get_all_process(device, pkgname):
     return [process for process in device.enumerate_processes() if pkgname in process.name]
 
 
-def search(api, args=None):
+def search(api):
     """
     """
 
@@ -54,36 +61,41 @@ def search(api, args=None):
     return matches
 
 
-def dump(pkg_name, api):
+def dump(pkg_name, api, mds=None):
     """
     """
+    if mds is None:
+        mds = []
     matches = api.scandex()
     for info in matches:
         try:
             bs = api.memorydump(info['addr'], info['size'])
+            md = md5(bs)
+            if md in mds:
+                click.secho("[DEXDump]: Skip duplicate dex {}<{}>".format(info['addr'], md), fg="blue")
+                continue
+            mds.append(md)
             if not os.path.exists("./" + pkg_name + "/"):
                 os.mkdir("./" + pkg_name + "/")
             if bs[:4] != b"dex\n":
                 bs = b"dex\n035\x00" + bs[8:]
             with open(pkg_name + "/" + info['addr'] + ".dex", 'wb') as out:
                 out.write(bs)
-            click.secho("[DEXDump]: DexSize={}, SavePath={}/{}/{}.dex"
-                        .format(hex(info['size']), os.getcwd(), pkg_name, info['addr']), fg='green')
+            click.secho("[DEXDump]: DexSize={}, DexMd5={}, SavePath={}/{}/{}.dex"
+                        .format(hex(info['size']), md, os.getcwd(), pkg_name, info['addr']), fg='green')
         except Exception as e:
             click.secho("[Except] - {}: {}".format(e, info), bg='yellow')
 
 
-def stop_other(pid, pkg_name):
-    processes = get_all_process(device, pkg_name)
-    if len(processes) > 1:
-        try:
-            for index in range(len(processes)):
-                if index == pid:
-                    os.system("adb shell \"su -c 'kill -18 {}'\"".format(processes[index].pid))
-                else:
-                    os.system("adb shell \"su -c 'kill -19 {}'\"".format(processes[index].pid))
-        except:
-            pass
+def stop_other(pid, processes):
+    try:
+        for process in processes:
+            if process.pid == pid:
+                os.system("adb shell \"su -c 'kill -18 {}'\"".format(process.pid))
+            else:
+                os.system("adb shell \"su -c 'kill -19 {}'\"".format(process.pid))
+    except:
+        pass
 
 
 def choose(pid=None, pkg=None, spawn=False, device=None):
@@ -118,20 +130,26 @@ if __name__ == "__main__":
         click.secho("[Except] - Unable to connect to device.", bg='red')
         exit()
 
-    pid = None
+    pid = -1
     pname = ""
     try:
-        pid, pname = choose()
+        pid, pname = choose(device=device)
     except Exception as e:
         click.secho("[Except] - Unable to inject into process: {} in \n{}".format(e, traceback.format_tb(
             sys.exc_info()[2])[-1]), bg='red')
-    if not pid:
         exit()
-    logging.info("[DEXDump]: found target [{}] {}".format(pid, pname))
-    session = device.attach(pid)
-    path = os.path.dirname(sys.argv[0])
-    path = path if path else "."
-    script = session.create_script(open(path + "/agent.js").read())
-    script.load()
 
-    dump(pname, script.exports)
+    processes = get_all_process(device, pname)
+    mds = []
+    for process in processes:
+        logging.info("[DEXDump]: found target [{}] {}".format(process.pid, process.name))
+        stop_other(process.pid, processes)
+        session = device.attach(process.pid)
+        path = os.path.dirname(sys.argv[0])
+        path = path if path else "."
+        script = session.create_script(open(path + "/agent.js").read())
+        script.load()
+        dump(pname, script.exports, mds=mds)
+        script.unload()
+        session.detach()
+    exit()
