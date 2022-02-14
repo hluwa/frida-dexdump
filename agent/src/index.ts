2d6d964806a6e872df0766f0c513e2a7ab655a70
hluwa <hluwa888@gmail.com>
2022-02-14 20:57:29 +0800

    mprotect target memory range.

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -6,14 +6,32 @@
 
 import {searchDex} from "./search";
 
+function setReadPermission(base: NativePointer, size: number) {
+    const end = base.add(size);
+    Process.enumerateRanges("---").forEach(function (range) {
+        const range_end = range.base.add(range.size)
+        if (range.base < base || range_end > end) {
+            return
+        }
+        if (!range.protection.startsWith("r")) {
+            console.log("Set read permission for memory range: " + base + "-" + range_end)
+            Memory.protect(range.base, range.size, "r" + range.protection.substr(1, 2))
+        }
+
+    })
+}
+
+
 rpc.exports = {
     memorydump: function (address, size) {
-        return new NativePointer(address).readByteArray(size);
+        const ptr = new NativePointer(address);
+        setReadPermission(ptr, size);
+        return ptr.readByteArray(size);
     },
     searchdex: function (enableDeepSearch: boolean) {
         return searchDex(enableDeepSearch);
     },
-    stopthreads: function(){
+    stopthreads: function () {
         Process.enumerateThreads().forEach(function (thread) {
 
         })
